9c5ea05c2d83b2b2a45f4030234cd98daaa7d649
jbedii <jbedi@akamai.com>
2024-12-23 16:01:46 +0530

    Jinja with for loop and using seperate var and template file

---
+++ Jinja-with-for-loop/render.py
@@ -0,0 +1,12 @@
+from jinja2 import Template
+import yaml
+import yamlloader
+
+
+with open("vars.yml", 'r') as d:
+	data = yaml.load(d, Loader=yamlloader.ordereddict.CLoader)
+
+with open('temp.j2') as t:
+	template = Template(t.read())
+
+print(template.render(data))
